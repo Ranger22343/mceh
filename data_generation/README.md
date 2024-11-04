8a4f7572da1e01c62b58d6130b0021779c572a2e
Hung-Yu Lin <ranger22343@gmail.com>
2024-11-04 15:46:56 +0800

    initial version

---
+++ data_generation/README.md
@@ -0,0 +1,69 @@
+Change Log of Data
+===
+
+[Toc]
+
+---
+# Introduction
+This markdown file contains the history of data change for Hung-Yu Lin's work. The python package `mceh`(created by Lin; its name is `mceh_verX` where "X" is the version) is used to generate new data. When a generating code is mentioned (named `generate_modified_xxx.ipynb` where xxx is the type of data), despite it is saved in `mceh/data_generation`, it should be ran in the same directory as `mceh` package. Therefore, the directory should looks like the following to run the generating code.
+```
+/mceh_verX
+/data
+    data_needed.fits
+generate_modified_xxx.ipynb
+```
+The `data_needed.fits` is the data needed for generating modified data and should be easily told in the code.
+# eFEDS
+## Original
+The original data is called `eFEDS_clusters_V3.2.fits` and `eFEDS_c001_main_ctp_clus_v2.1.fits`, which is from [the eFEDS catalogue website](https://erosita.mpe.mpg.de/edr/eROSITAObservations/Catalogues/) and their IDs are 11 and 12 (`eFEDS EXT Clusters cat` and `eFEDS EXT CTP cat` buttons).
+
+## eFEDS Ver.6
+The data name is `modified_efeds_ver6.fits`.
+The generating code is `generate_modified_efeds_ver6.fits`.
+`modified_random_ver1.fits`, `modified_hsc_ver1.fits`, `eFEDSmasstable.fits`, `bc03_rs_zfp3d0_tau_p0d4.fits`(from I-Non) and `eFEDS_c001_main_ctp_clus_v2.1.fits` are needed.
+The modifications of the previous versions are unclear, so they are not recorded in this markdown file. This new version of eFEDS file will be generated independently of the previous modified versions.
+The modifications are:
+- Only columns named `Name`, `ID_SRC`, `RA_CORR`, `DEC_CORR` and `Z_BEST_COMB` remain from `eFEDS_c001_main_ctp_clus_v2.1.fits`.
+- Add a column named `low_cont_flag`, which is `True` when the clusters meet the following conditions (from [Chiu+23](https://arxiv.org/abs/2207.12429) section 2.1): 
+1. `DET_LIKE` > 5
+2. `EXT_LIKE` > 6
+3. `EXT` > 0
+4. `F_CONT_BEST_COMB` < 0.3
+5.  0.1 < `Z_BEST_COMB` < 1.2
+- Add columns `r500c_lcdm` and `median_500c_lcdm` from `eFEDSmasstable.fits`. Note that unit of `median_500c_lcdm` has changed to better suit the units of astropy.
+- Add columns `unmasked_area` and `area`. The unmasked area is obtained by counting numbers (#) of random points of random catlog within the radii (`r500c_lcdm`) of the cluter centers, so it is #/100 $\text{arcmin}^2$. And `area` is the actual areas of the clusters on the sky, which are obtained by $2\pi (1-\cos^2{r})$.
+- Add columns `g/r/i/z/ymag_cmodel` which are the characteristic magnitude from I-Non's model.
+- Add the column `galaxy_index` which is the  indicies of galaxies (from `modified_hsc_ver1.fits`) within the radii (`r500c_lcdm`) of the cluster centers.
+
+# HSC
+## Original
+The original data is called `429561.fits`, which is from [the HSP-SSP data release website](https://hsc-release.mtk.nao.ac.jp/doc/index.php/data-access__pdr3/). Use the `CAS Search` entry and the SQL (`hsc.sql`) is saved in the same directory as this markdown file.
+
+## HSC Ver.1
+The data name is `modified_hsc_ver1.fits`.
+The generating code is `generate_modified_hsc_ver1.ipynb`.
+`429561.fits` is needed.
+The modifications are:
+- Only rows with `science_flag == True` remain.
+- Only rows with `iclassification_extendedness > 0.9` remain.
+- Only columns named `object_id`, `tract`, `ra`, `dec`, `gmag_cmodel`, `rmag_cmodel`, `imag_cmodel`, `zmag_cmodel` and `ymag_cmodel` remain.
+- The columns `ra` and `dec` have a unit degree.
+
+# Random
+
+## Original
+The random data is `random_gama09h.fits`, which directly links to `/array/users/profoak/dataDir/hsc/pdr3/random/random_gama09h.fits` in Pikachu.
+
+## Random Ver.1
+
+The data name is `modified_random_ver1.fits`. 
+The generating code is called `generate_modified_random_ver1.ipynb`.
+`random_gama09h.fits` is needed.
+The modifications are:
+- Only save the `RA` and `DEC` columns.
+- Add the unit `degree` to these two columns.
+- Only rows with `science_flag == True` are saved.
+
+# eFEDSmasstable
+
+The data is called `eFEDSmasstable.fits` and it's from [I-Non's website](https://inonchiu.github.io/eFEDScosmology_chiu22/).
---
+++ mceh/find_luminosity.py
@@ -0,0 +1,343 @@
+"""
+Last changed date: 2024 Oct 26
+"""
+# %%
+import random
+from astropy.table import Table
+import numpy as np
+from astropy.cosmology import Planck18 as cosmo
+from astropy import coordinates as coord
+from astropy.io import fits
+from astropy.io import ascii
+from astropy import units as u
+from matplotlib import pyplot as plt
+from itertools import accumulate
+import copy
+import lzma
+
+def index_within_angular_radius(sat_ra, sat_dec, center_ra, center_dec,
+                                angular_radius):
+    """Find the index of data within a given circle on the sky.
+
+    Return a list of object indices which locate within a circle at 
+    [`center_ra`, `center_dec`] with a radius `angular_radius`.
+
+    Args:
+        sat_ra (Quantity): RA of objects whose indices are you want to find.
+        sat_dec (Quantity): DEC of objects whose indices are you want to find.
+        center_ra (Quantity): RA of the cirlce center.
+        center_dec (Quantity): DEC of the circle center.
+        angular_radius (Quantity): Radius of the circle.
+
+    Returns:
+        list: List of indicies whose corresponding positions are within the 
+            cirlce.
+    """
+    clean_index = clean_skypoint(center_ra, center_dec, angular_radius, sat_ra, 
+                                 sat_dec, return_index=True)
+    clean_sat_ra, clean_sat_dec = sat_ra[clean_index], sat_dec[clean_index]
+    sat_coord = coord.SkyCoord(clean_sat_ra, clean_sat_dec)
+    cen_coord = coord.SkyCoord(center_ra, center_dec)
+    angular_distance = cen_coord.separation(sat_coord)
+    in_radius = angular_distance <= angular_radius
+    returnme = clean_index[in_radius]
+    return returnme
+
+
+def is_within_angular_radius(sat_ra, sat_dec, center_ra, center_dec,
+                             angular_radius):
+    """Find the index of data within a given circle on the sky.
+
+    Return a Boolean list states if the input object locates within a circle at 
+    [`center_ra`, `center_dec`] with a radius `angular_radius`.
+
+    Args:
+        sat_ra (Quantity): RA of objects whose indices are you want to find.
+        sat_dec (Quantity): DEC of objects whose indices are you want to find.
+        center_ra (Quantity): RA of the circle center.
+        center_dec (Quantity): DEC of the circle center.
+        angular_radius (Quantity): Radius of the circle.
+    
+    Returns:
+        list: Boolean list which states whether the corresponding objects are
+            within the circle.
+    """
+    sat_coord = coord.SkyCoord(sat_ra, sat_dec)
+    cen_coord = coord.SkyCoord(center_ra, center_dec)
+    angular_distance = cen_coord.separation(sat_coord)
+    in_radius = angular_distance <= angular_radius
+    return in_radius
+
+
+def satellites_around_centers(sat_data, cen_ra, cen_dec, cen_r):
+    """Remove the data which is not in the circles with given positions/radii.
+    
+    The element in `sat_data` will be removed if it does not locate in the
+    circles at [`cen_ra`, `cen_dec`] with angular radii `cen_r` on the sky.
+    Note that there must be keys 'RA' and 'DEC' in `sat_data` with units.
+
+    Args:
+        sat_data (QTable): The table with elements which you want to delete if
+            not within the circles. It must contains the keys `RA` and `DEC`.
+        cen_ra (Quantity): RA of the circles.
+        cen_dec (Quantity): DEC of the circles.
+        cen_r (Quantity): Angular radii of the cirlces.
+
+    Returns:
+        QTable: The table after the deletion.
+    """
+    qualified_index = []
+    cen_num = len(cen_ra)
+    for i in range(cen_num):
+        qualified_index.extend(
+            index_within_angular_radius(sat_data['RA'], sat_data['DEC'],
+                                        cen_ra[i], cen_dec[i], cen_r[i]))
+        print(f'finish {i+1}/{cen_num}')
+    qualified_index = np.unique(qualified_index)
+    sat_data = sat_data[qualified_index]
+    return sat_data
+
+
+def quantity_function(quantity_name, center_position, satellites_data,
+                      angular_radius, bins):
+    """Distribution of a given quantity of the satellites around given centers.
+    
+    It will find the objects in `satellites_data` which are around the 
+    `center_position` within raii `angular_radius` on the sky, and uses the key
+    `quantity_name` of the objects to give its hisogram with `bins`.
+
+    Args:
+        quantity_name (str): Name of the quantity which contructs the
+            distribution. It should be containd in `satellites_data`.
+        center_position (array Quantity): [RA, DEC] of the centers.
+        satellites_data (QTable): Data of the satellites. Must contains keys
+            'RA', 'DEC' and `quantity_name`.
+        angular_radius (Quantity): Radii of the circles around the centers.
+        bins (int or array-like): Bins of the histogram. It can be the number of
+            bins or their boundaries.
+
+    Returns:
+        hist (array): Histogram of the quantity.
+        bins_edge (array): Edges of the distribution bins.  
+    """
+    sat_ra, sat_dec = satellites_data['RA'], satellites_data['DEC']
+    cen_ra, cen_dec = center_position[0], center_position[1]
+    index_r = index_within_angular_radius(sat_ra, sat_dec, cen_ra, cen_dec,
+                                          angular_radius)
+    satellites_data = satellites_data[index_r]
+    satellites_data = satellites_data[~np.isnan(satellites_data[quantity_name]
+                                                )]
+    satellites_data = satellites_data[~np.isinf(satellites_data[quantity_name]
+                                                )]
+    hist, hist_bins = np.histogram(satellites_data[quantity_name], bins=bins)
+    return hist, hist_bins
+
+
+def count_number(position, radius, sat_data):
+    '''Count the number of objects in given circles.
+    
+    Count the number of objects of `sat_data` within the cirlces with `position`
+    and `radius` on the sky. The position informaiton should be contained in 
+    `sat_data`.
+
+    Args:
+        position (array Qunatity): [RA, DEC] of the circles.
+        radius (Quantity): Radii of the circles.
+        sat_data (QTable): Table of the objects to be counted. Must contains
+            keys 'RA' and 'DEC'.
+    
+    Returns:
+        int: Number of objects in `sat_data` which locates within the cirlces.
+
+    '''
+    cen_ra, cen_dec = position
+    sat_ra, sat_dec = clean_skypoint(cen_ra, cen_dec, radius, sat_data['RA'], 
+                                     sat_data['DEC'])
+    number = len(
+        index_within_angular_radius(sat_ra, sat_dec, cen_ra, cen_dec, radius))
+    return number
+
+
+def index_between_angular_radius(data, pos, r_in, r_out):
+    """Find which index has distance to center between `r_in` and `r_out`.
+
+    Return the index whose data has distance to the center between `r_in` 
+    and `r_out`.
+
+    Args
+    ----
+    data: satellite data with 'ra' and 'dec' keys.
+    pos: position of the center which has the form [ra, dec]. 
+    r_in: inner radius.
+    r_out: outer radius.
+    
+    Returns
+    -------
+    An index array whose data has distance between `r_in` and `r_out`.
+    """
+    sat_ra, sat_dec = data['ra'], data['dec']
+    distance = coord.angular_separation(
+        pos[0] * np.pi / 180, pos[1] * np.pi / 180, sat_ra * np.pi / 180,
+        sat_dec * np.pi / 180) * 180 / np.pi
+    index = np.where((r_in <= distance) & (distance <= r_out))[0]
+    return index
+
+
+def local_bkg(pos,
+              r_in,
+              r_out,
+              bins,
+              area_in,
+              hsc,
+              random,
+              quantity='imag_cmodel',
+              bkg_coverage=False):
+    """The local background of the cluster.
+    
+    Return the local background of quantity `quantity`. If coverage == True,
+    also return the covered percent (masked area / actual area).
+
+    Args
+    ----
+    pos: position of the center.
+    r_in: inner radius of the local background.
+    r_out: outer radius of the local background.
+    bins: the bin bounds which sort `quantity`.
+    area_in: the area in interested.
+    hsc: hsc data.
+    random: random data.
+    bkg_coverage: if True, return a list with [original result, covered percent].
+
+    Returns
+    -------
+    An array which indicates the local background with `bins`. If
+    bkg_coverage == True, returns a list whose the first element is the local
+    backgroudn and the second one is the covered percentof the background.
+    """
+    hsc_i = index_between_angular_radius(hsc, pos, r_in, r_out)
+    hsc = hsc[hsc_i]
+    bkg = np.histogram(hsc[quantity], bins)[0]
+    actual_bkg_area = (r_out**2 - r_in**2) * np.pi
+    bkg = bkg / actual_bkg_area * area_in  # normalized background
+    if bkg_coverage == True:
+        cover_num = index_between_angular_radius(random, pos, r_in, r_out)
+        cover_area = cover_num / 100 / 3600
+        cover_percent = cover_area / actual_bkg_area
+        return [bkg, cover_percent]
+    return bkg
+
+
+def unmasked_area(ra, dec, r, rd):
+    # Calculate the unmasked area of a given circle.
+    pos = [ra, dec]
+    num = count_number(pos, r, rd)
+    return num / 100 * u.arcmin**2
+
+
+def hsc_photoz(z, hsc):  # Apply photo-z selction on hsc based on the redshift.
+    return hsc[(hsc['zbound'][:, 0] < z) & (hsc['zbound'][:, 1] > z)]
+
+
+def nonoverlapping_random_point(number,
+                                r,
+                                ra_min,
+                                ra_max,
+                                dec_min,
+                                dec_max,
+                                prevent_ra,
+                                prevent_dec,
+                                prevent_r,
+                                try_limit=10000,
+                                rd_data=None,
+                                coverage=0.,
+                                progress=False,
+                                step=False):
+    """Generate a given number of non-overlapping random points on the sky.
+    
+    `number` of points with radii `r` will be generated without touching each 
+    others, avoiding circular area with positions [`prevent_ra`, 
+    `prevent_dec`] and radii `prevent_r`. Note that the random RA are uniformly
+    chose between [`ra_min`, `ra_max`] while the DEC are [sin(`dec_min`), 
+    sin(`dec_max`)]. You can choose the percentage threshold of the unmasked
+    area by setting `coverage` to a number. 
+
+    Args:
+        number (int): Number of random points to be generated.
+        r (scalar Quantity): The radius of the random points.
+        ra_min (scalar Quantity): The minimal RA for generating random points.
+        ra_max (scalar Quantity): The maximal RA for generating random points.
+        dec_min (scalar Quantity): The minimal DEC for generating random points.
+        dec_max (scalar Quantity): The maximal DEC for generating random points.
+        prevent_ra (Quantity): The center RA of the preventing areas.
+        prevent_dec (Quantity): The center DEC of the preventing areas.
+        prevent_r (Quantity): The radii of the preventing areas.
+        try_limit (int): Maximal try of getting a qualified point.
+        rd_data (QTable): Random data for calculating the area. Must contains
+            keys 'RA' and 'DEC'.
+        coverage (float): The lowest percentage of unmasked area to pass.
+        progress (bool): Whether it should print the progress.
+        step (bool): Whether it should print the finish of each try.
+    
+    Returns:
+        ((`number`,) Quantity, (`number`,) Quantity)
+        The RA/DEC of the random points.
+    """
+    pass_ra = []
+    pass_dec = []
+    for i in range(number):
+        prevent_coord = coord.SkyCoord(prevent_ra, prevent_dec)
+        tried = 0
+        while tried < try_limit:
+            if step == True:
+                print(f'try {tried+1}/{try_limit}')
+            # Generate a random point.
+            rd_ra = random.uniform(
+                ra_min.to(u.deg).value,
+                ra_max.to(u.deg).value) * u.deg
+            rd_dec = np.arcsin(
+                random.uniform(np.sin(dec_min.to(u.rad).value),
+                               np.sin(dec_max.to(
+                                   u.rad).value))) / np.pi * 180 * u.deg
+            rd_coord = coord.SkyCoord(rd_ra, rd_dec)
+
+            sep = rd_coord.separation(prevent_coord)
+            if np.all((sep > prevent_r + r)):
+                if coverage != 0:
+                    area = r**2 * np.pi
+                    unm_area = unmasked_area(rd_ra, rd_dec, r, rd_data)
+                    cov = unm_area / area
+                    if cov < coverage / 100:
+                        tried += 1
+                        continue
+                pass_ra.append(rd_ra)
+                pass_dec.append(rd_dec)
+                prevent_ra = np.append(prevent_ra, rd_ra)
+                prevent_dec = np.append(prevent_dec, rd_dec)
+                prevent_r = np.append(prevent_r, r)
+                break
+            tried += 1
+        if tried >= try_limit:
+            print(f'WARNING: Exceed the trying limit at n = {len(pass_ra)}.'
+                  ' Return the left coordinates.')
+            break
+        if progress == True:
+            print(f'Finish {i+1}/{number}')
+    pass_ra = u.quantity.Quantity(pass_ra)
+    pass_dec = u.quantity.Quantity(pass_dec)
+    return pass_ra, pass_dec
+
+
+def clean_skypoint(ra, dec, r, data_ra, data_dec, return_index=False):
+    ddec = r
+    if np.abs(dec + r) < np.abs(dec - r):
+        r = -r
+    #TODO: Make it compatiable when the boundaries meet the max/min ra/dec. 
+    max_dra = (np.arccos((np.cos(r) - 1) / (np.cos(dec + r)**2) + 1))
+    min_ra, max_ra = ra - max_dra, ra + max_dra
+    min_dec, max_dec = dec - ddec, dec + ddec
+    is_ra = (data_ra > min_ra) & (data_ra < max_ra)
+    is_dec = (data_dec > min_dec) & (data_dec < max_dec)
+    is_return = is_ra & is_dec
+    if return_index:
+        return np.where(is_return)[0]
+    return data_ra[is_return], data_dec[is_return]
